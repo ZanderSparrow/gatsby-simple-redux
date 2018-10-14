1fa771bbcc7733fb5a721b978da5e05740123e51
Silverhawk <sondra.silverhawk@hush.com>
2018-10-14 12:33:45 -0700

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Simple Redux Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'simple-redux-starter',
+        short_name: 'simple-redux',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/bird.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
